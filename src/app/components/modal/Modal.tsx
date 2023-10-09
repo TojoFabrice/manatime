4bcfc9773f2f4d3cbf04bd52c020456514abc133
Tojo <fabricetojo29@gmail.com>
2023-10-09 17:33:16 +0300

    suite modal

--- src/app/components/modal/Modal.tsx
+++ src/app/components/modal/Modal.tsx
@@ -2,6 +2,8 @@
 
 import { useVisibilityContext } from '@/app/context/VisibilityContext';
 import React from 'react'
+import Image from 'next/image'
+import logo from '../../assets/icons/Logo.svg'
 
 const Modal = () => {
     const { isVisible, setIsVisible } = useVisibilityContext();
@@ -10,7 +12,12 @@ const Modal = () => {
         <div className='absolute inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-start'>
             <div className='w-1/2 h-screen '>
                 <div className='bg-white p-4 h-screen text-black rounded-r-2xl'>
-                    Modal
+                    <div className='flex justify-center mt-[118px]'>
+                        <Image src={logo} alt="Logo" width={350} height={72.5}/>
+                    </div>
+                    <div className='flex justify-center'>
+                        menu
+                    </div>
                 </div>
             </div>
         </div>
